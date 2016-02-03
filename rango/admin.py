9489fd1553c4653cd15eeb234dd0820ac5de198a
Martin Grant <martingrant@outlook.com>
2016-02-03 22:09:35 +0000

    Up to chapter 6.9

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
+
+
+
+#admin.register(Page, PageAdmin)
