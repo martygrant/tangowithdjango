a0c9971dbd06b722ea58d191210cd4154e62208b
Martin Grant <martingrant@outlook.com>
2016-02-04 20:47:32 +0000

    Initial commit.

--- rango/admin.py
+++ rango/admin.py
@@ -3,10 +3,12 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.register(Page, PageAdmin)
+admin.site.register(Category)
+#admin.site.register(Page)
+
+
+
+admin.site.register(Page, PageAdmin)
