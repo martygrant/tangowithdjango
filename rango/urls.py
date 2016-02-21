06ad5fc83e9e4d5d343b561008bd83ffa258b8c4
Martin Grant <Marty@Martins-MacBook-Pro.local>
2016-02-21 17:06:29 +0000

    Completed up to 8.2.6

--- rango/urls.py
+++ rango/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 urlpatterns = patterns('',
         url(r'^$', views.index, name="index"),
         url(r'about', views.about, name="about"),
+        url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)  # New!
 
 
